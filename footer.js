0fac92b3d74e074cfd445fa7d04799ed347398a7
CHRISTIAN FARBER <FARBERSTYLE@GMAIL.COM>
2025-11-28 17:31:12 -0800

    Scroll-controlled carousel, centered footer with large seal

--- footer.js
+++ footer.js
@@ -1,6 +1,6 @@
 /**
  * FOOTER.JS - Institutional Footer with Seal
- * Two column groups with seal in center + Copyright
+ * Two centered columns with large seal in middle
  */
 
 (function() {
@@ -10,37 +10,27 @@
     footer.innerHTML = `
       <div class="footer-container">
         <div class="footer-main">
-          <div class="footer-left-columns">
-            <div class="footer-column">
-              <h4>Organization</h4>
-              <ul>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="careers.html">Careers</a></li>
-              </ul>
-            </div>
-            
-            <div class="footer-column">
-              <h4>Resources</h4>
-              <ul>
-                <li><a href="verify.html">Verify License</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="advocacy.html">Advocacy</a></li>
-              </ul>
-            </div>
+          <div class="footer-column">
+            <h4>Organization</h4>
+            <ul>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="careers.html">Careers</a></li>
+              <li><a href="contact.html">Contact</a></li>
+            </ul>
           </div>
           
           <div class="footer-seal-center">
             <img src="gold-seal.png" alt="Holistic Therapy Dog Association Seal" />
           </div>
           
-          <div class="footer-right-columns">
-            <div class="footer-column">
-              <h4>Legal</h4>
-              <ul>
-                <li><a href="privacy-policy.html">Privacy Policy</a></li>
-                <li><a href="terms.html">Terms & Conditions</a></li>
-              </ul>
-            </div>
+          <div class="footer-column">
+            <h4>Legal</h4>
+            <ul>
+              <li><a href="verify.html">Verify License</a></li>
+              <li><a href="privacy-policy.html">Privacy Policy</a></li>
+              <li><a href="terms.html">Terms & Conditions</a></li>
+              <li><a href="advocacy.html">Advocacy</a></li>
+            </ul>
           </div>
         </div>
         
